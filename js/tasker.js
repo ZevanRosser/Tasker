6b45412c8a5b40c6c672c8132ad32ccfbaaeac70
Zevan Rosser <bendvent@gmail.com>
2012-05-18 09:17:30 +0900

    adding first version

---
+++ js/tasker.js
@@ -0,0 +1,138 @@
+$(function() {
+
+  var doc = $(document),
+      win = $(window);
+
+  var tasker = new Tasker();
+
+  function Tasker() {
+    var self = this;
+    self.elem = $("#main-ui").selectAll(self).appendTo("body");
+
+    self.update = function() {
+      win.trigger("arrange");
+    };
+    
+    self.save = function() {
+      localStorage.tasker = self.taskContainer.html();
+      self.update();
+    };
+
+    if (localStorage.tasker) {
+      self.taskContainer.html(localStorage.tasker);
+    }
+
+    self.addForm.submit(function(e) {
+      e.preventDefault();
+      var value = $.trim(self.title.val());
+      if (value.length == 0) return;
+      var taskList = new TaskList(value);
+      self.title.val("");
+      taskList.elem.prependTo(self.taskContainer);
+      taskList.newTask.focus();
+      win.trigger("resize");
+      self.save();
+    });
+  };
+
+  function TaskList(title) {
+    this.elem = $("#task-list").selectAll(this);
+    this.title.text(title);
+  };
+  doc.on("submit", ".add-form", function(e) {
+    e.preventDefault();
+    var curr = $(this),
+        parent = curr.parent(),
+        container = parent.find(".tasks"),
+        text = curr.find(".new-task"),
+        value = $.trim(text.val());
+    if (value.length == 0) return;
+
+    var task = new Task(value);
+    task.elem.appendTo(container);
+    task.check.trigger("evalchecks");
+    text.val("");
+    parent.css({
+      backgroundColor: "white"
+    });
+    tasker.save();
+  }).on("click", ".close", function() {
+    if (confirm("Are you sure you want to remove this task list?")) {
+      $(this).parent().remove();
+      tasker.save();
+    }
+  });
+
+  function Task(text) {
+    this.elem = $("#task").selectAll(this);
+    this.text.text(text);
+  };
+  doc.on("click evalchecks", ".check", function() {
+    var curr = $(this),
+        parent = curr.parent().parent(),
+        taskList = parent.parent(),
+        feedback = taskList.find(".feedback"),
+        checks = parent.find(".check"),
+        checkNum = checks.length,
+        checked = 0;
+    taskList.css({
+      backgroundColor: "white"
+    });
+    checks.each(function() {
+      var check = $(this);
+      if (check.prop("checked")) {
+        checked++;
+        check.attr("checked", true);
+      } else {
+        check.attr("checked", false);
+      }
+      if (checked == checkNum) {
+        taskList.css({
+          backgroundColor: "#c5ebdd"
+        });
+      }
+    });
+    feedback.text(checked + "/" + checkNum + " completed");
+    tasker.save();
+  }).on("click", ".little-btn", function() {
+    if (confirm("Are you sure you want to remove this task?")) {
+      $(this).parent().remove();
+      tasker.save();
+    }
+  });
+
+  win.on("resize arrange", function() {
+    var taskLists = $("#task-container .task-list");
+    var size = taskLists.eq(0).outerWidth() + 5;
+    var num = taskLists.length;
+    var winWidth = win.width();
+    var cols = Math.floor(winWidth / size);
+    var margin = 5;
+    var top = 20;
+    taskLists.css({
+      left: margin,
+      top: top
+    });
+    for (var i = 1; i < num; i++) {
+      var pre = taskLists.eq(i - 1);
+      var curr = taskLists.eq(i);
+      var prePos = pre.position();
+      curr.css({
+        top: top,
+        left: prePos.left + pre.outerWidth() + margin
+      });
+
+      if (i >= cols) {
+
+        var above = taskLists.eq(i - cols);
+        console.log(above.find("span").text(), i);
+        var abovePos = above.position();
+        curr.css({
+          left: abovePos.left,
+          top: abovePos.top + above.outerHeight() + margin
+        });
+      }
+    }
+  }).trigger("resize");
+
+});
